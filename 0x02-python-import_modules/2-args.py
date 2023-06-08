eea82e31a14b534c872db9cc05205086296aa21a
PITSO98 <sqinisekomrbe@gmail.com>
2023-06-08 03:42:38 -0700

    task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    for j in range(i):
+        print("{}: {}".format(j + 1, sys.argv[j + 1]))
