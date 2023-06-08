e38f4136964c50b213926450f7424429e1600064
PITSO98 <sqinisekomrbe@gmail.com>
2023-06-07 17:40:28 -0700

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if c == not( ord('e') and  ord('q'):
+    if not c == (ord('e') and  ord('q')):
         print("{:c}".format(c), end="")
