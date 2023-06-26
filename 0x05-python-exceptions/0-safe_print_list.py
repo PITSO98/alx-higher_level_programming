9fd095f30798a2b56b463f583d016e49ac169163
PITSO98 <sqinisekomrbe@gmail.com>
2023-06-26 16:41:45 -0700

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""P"""
 def safe_print_list(my_list=[], x=0):
     c = 0
     for i in range( x):
@@ -7,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
             c += 1
         except:
             break
-    print('')
-    return c
+    print("")
+    return (c)
