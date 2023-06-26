f2326d82d5b40770d1ef9d687a831d6134d3c9a7
PITSO98 <sqinisekomrbe@gmail.com>
2023-06-26 16:29:07 -0700

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             c += 1
         except:
-            continue
+            break
     print('')
     return c
