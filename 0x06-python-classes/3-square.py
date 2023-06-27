4cfab5d57e3546a2e55b03b657d7becdffdd7851
PITSO98 <sqinisekomrbe@gmail.com>
2023-06-27 16:00:59 -0700

    task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,6 +14,7 @@ class Square:
             self.__size = size
 
     def area(self):
-        return self.__size * self.__size
+        """Return the current area of the square."""
+        return (self.__size **2)
 ~                                                                                                                                                    
 ~                                         
