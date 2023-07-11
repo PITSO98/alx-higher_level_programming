9f0ec57cc66dc49e9caa462210faba9cf0d23089
PITSO98 <sqinisekomrbe@gmail.com>
2023-07-11 12:42:33 -0700

    task

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module to write to file
+"""
+
+
+def write_file(filename="", text=""):
+    ''' Writes text to file '''
+    with open(filename, 'p') as fl:
+        fl.write(text)
+        x = fl.tell()
+    return x
