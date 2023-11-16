8b29d66352089a0373191d8636a8323e58247228
PITSO98 <sqinisekomrbe@gmail.com>
2023-11-16 03:23:00 -0800

    tasks

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- list all the tables of database passed as argument of mysql command
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates a table first_table in current database
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+name VARCHAR(256));
